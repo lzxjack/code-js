82fba412c2e970f577e63acafa7e3055a6e438c6
Jack <965555169@qq.com>
2021-10-25 10:24:51 +0800

    update

---
+++ code-js/17-判断数据类型.js
@@ -0,0 +1,15 @@
+const getType = obj => {
+    return typeof obj === 'object'
+        ? Object.prototype.toString.call(obj).replace('[object ', '').replace(']', '').toLowerCase()
+        : typeof obj;
+};
+
+console.log(getType(123)); // number
+console.log(getType('string')); // string
+console.log(getType(true)); // boolean
+console.log(getType(undefined)); // undefined
+console.log(getType(null)); // null
+console.log(getType({})); // object
+console.log(getType([])); // array
+console.log(getType(/123/)); // regexp
+console.log(getType(new Date())); // date
