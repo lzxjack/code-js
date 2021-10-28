a16160ee4e5cf8bfb816fbcef5ade16db9675bb8
Jack <965555169@qq.com>
2021-10-28 15:39:05 +0800

    update

---
+++ code-js/20-实现instanceof.js
@@ -0,0 +1,25 @@
+function Person() {}
+function Person2() {}
+
+const usr = new Person();
+
+function myInstanceof(obj, constructor) {
+    // obj的隐式原型
+    let implicitPrototype = obj.__proto__;
+    // 构造函数的原型
+    const displayPrototype = constructor.prototype;
+    // 遍历原型链
+    while (implicitPrototype) {
+        // 找到，返回true
+        if (implicitPrototype === displayPrototype) return true;
+        implicitPrototype = implicitPrototype.__proto__;
+    }
+    // 遍历结束还没找到，返回false
+    return false;
+}
+
+console.log(myInstanceof(usr, Person)); // true
+console.log(myInstanceof(usr, Object)); // true
+
+console.log(myInstanceof(usr, Person2)); // false
+console.log(myInstanceof(usr, Function)); // false
