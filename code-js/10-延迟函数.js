e68652b4aad1661eee28f7ac6bd38354b209d11a
Jack <965555169@qq.com>
2021-12-20 19:51:19 +0800

    update

--- code-js/10-延迟函数.js
+++ code-js/10-延迟函数.js
@@ -1,5 +1,11 @@
-// 实现一个sleep()延迟函数，例如，sleep(2000)表示等待2000毫秒，再执行函数里面的操作。
-
 const sleep = time => {
     return new Promise(resolve => setTimeout(resolve, time));
 };
+
+console.log(1);
+
+sleep(2000).then(() => {
+    console.log(2);
+});
+
+console.log(3);
